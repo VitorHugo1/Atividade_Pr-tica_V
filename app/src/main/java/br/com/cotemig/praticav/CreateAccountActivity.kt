a944b3a4ff96475de7158d57334f01cae447ce8e
Vitor <71900055@faculdadecotemig.br>
2021-09-09 18:53:49 -0300

    Código Create Conta

--- app/src/main/java/br/com/cotemig/praticav/CreateAccountActivity.kt
+++ app/src/main/java/br/com/cotemig/praticav/CreateAccountActivity.kt
@@ -8,4 +8,29 @@ class CreateAccountActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
     }
+
+ @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.CreateAccountActivity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Cadastros");
+        linkToXML();
+
+        btnCadCategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(Cadastros.this, CategoriaLista.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(Cadastros.this, ClienteLista.class);
+                startActivity(intent);
+            }
+        });
 }
