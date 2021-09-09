c5b7e2baf980bf6267a6db37c3bc34f394f29f57
Vitor <71900055@faculdadecotemig.br>
2021-09-09 19:07:27 -0300

    Código Consultar saldo

--- app/src/main/java/br/com/cotemig/praticav/BalanceCheckActivity.kt
+++ app/src/main/java/br/com/cotemig/praticav/BalanceCheckActivity.kt
@@ -8,4 +8,34 @@ class BalanceCheckActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_balance_check)
     }
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conta_detalhes);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        linkToXml();
+
+        intent = getIntent();
+        if (intent != null) {
+            params = intent.getExtras();
+            if (params != null) {
+                idConta = params.getInt("id");
+            }
+        }
+
+        preencherDetalhes(getDadosConta());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(ContaDetalhes.this, ContaEditar.class);
+                params.putInt("id", idConta);
+                intent.putExtras(params);
+                startActivity(intent);
+            }
+        });
+    }
 }
